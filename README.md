cb26c0e0baa0d3b0b4ebdc1d190fd6105f80455b
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-03-03 10:57:42 -0700

    updated the read me, Coding out concert and spotify

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-#Description
+# Description
 
 Liri is a Node.js app that depends on user input from the command line
 Integrated Twitter, Spotify, and OMDb APIs return information based in specific user input
 The following specific commands will return searc results from the API’s:
 
-1.liri node “spotify-this-song” and a “song-name” - to return search results based on the song-name entry.
-2.liri node “movie-this” and a “movie-name” - to return search results based on the movie-name entry.
-3.liri node “do-what-it-says” - to access and execute the data command in the random.txt file.
+1. liri node “spotify-this-song” and a “song-name” - to return search results based on the song-name entry.
+2. liri node “movie-this” and a “movie-name” - to return search results based on the movie-name entry.
+3. liri node “do-what-it-says” - to access and execute the data command in the random.txt file.
 
-#Technologies Used
+# Technologies Used
 Node.js
 JavaScript
 Spotify API (via spotify npm module)
 OMDb API (via request npm module)
 
-#You’ll need these Node packages to run this app in the command line
+# You’ll need these Node packages to run this app in the command line
 
 Axios: https://www.npmjs.com/package/axios
 Node-Spotify-API: https://www.npmjs.com/package/node-spotify-api
 DotEnv: https://www.npmjs.com/package/dotenv
+
+
+http://www.omdbapi.com/?apikey=[258eb148]&
--- liri.js
+++ liri.js
@@ -13,10 +13,35 @@ var command = process.argv[2];
 var request = require("request");
 
 //pull for the concert
-
 if (command === "concert-this") {
     var artist = process.argv[3];
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, resopnse, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("-------------------------");
+            console.log("Venue: " + JSON.parse(body)[0].venue.name)
+            console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
+            console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
+            console.log("-------------------------");
+        }
+    });
+} else if (command === "spotify-this-song") {
+    var song = process.argv[3];
+    if (song === undefined) {
+        song = "The Sign";
+    }
 
-    })
-});
+    spotify.search({
+        type: "track",
+        query: song
+    }, function(err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
+        }
+        console.log("---------------------");
+        console.log("Artist: " + data.tracks.items[0].artist.name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("---------------------");
+    });
+}
